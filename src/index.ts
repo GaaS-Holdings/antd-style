3edd2ef2bbfd6aa40d69d365fde0d2f72d6a8af5
Pavel Alexeichik <pavel.alexeichik@gmail.com>
2023-12-04 20:36:44 +0100

    Archive: improve createStyles performance

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from './factories/createThemeProvider/type';
 export * from './functions';
 export * from './hooks';
 export * from './types';
+
+export { useMediaQueryMap } from './factories/createStyles/response'
+export { FasterAntdStyleContext } from './factories/createStyles'
