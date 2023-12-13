ab350d5c072b51d51cbe6ce101f37c30183b8cad
Pavel Alexeichik <pavel.alexeichik@gmail.com>
2023-12-13 12:04:05 +0100

    release

---
+++ es/factories/createStyles/FasterAntdStyleProvider.d.ts
@@ -0,0 +1,9 @@
+import { type Context, type ReactNode } from 'react';
+export declare const FasterAntdStyleContext: Context<{
+    cache?: any;
+    theme?: any;
+    responsiveMap?: any;
+}>;
+export declare const FasterAntdStyleProvider: ({ children }: {
+    children: ReactNode;
+}) => import("react/jsx-runtime").JSX.Element;
